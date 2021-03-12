f837a01949423a40fdc65d13cb58706ed99fb8b5
Diony <dionybusker@gmail.com>
2021-03-12 11:54:56 +0100

    Go to the next and previous statements

--- js/script.js
+++ js/script.js
@@ -4,14 +4,16 @@ const statementContainer = document.getElementById("statementContainer");
 const startButton = document.getElementById("startButton");
 const title = document.getElementById("title");
 const statement = document.getElementById("statement");
-const nextButton = document.getElementById("nextButton");
+const buttons = document.querySelectorAll(".buttons");
 
 // Variables
 var currentSubject = 0;
 
 startButton.onclick = changeView;
-nextButton.onclick = switchStatement;
 
+buttons.forEach(element => {
+    element.onclick = switchStatement;
+})
 
 // Add Bootstrap class "d-none"
 function hide(element) {
@@ -40,11 +42,39 @@ function changeContent(currentSubject) {
 // Switch between multiple statements
 function switchStatement() {
     switch (this.innerHTML) {
-        case "Next":
-            currentSubject++;
-            changeContent(currentSubject);
+        case "Ga terug":
+            console.log("Ga terug");
+            goToPreviousStatement();
+            break;
+        case "Eens":
+            console.log("Eens");
+            goToNextStatement();
+            break;
+        case "Geen van beide":
+            console.log("Geen van beide");
+            goToNextStatement();
+            break;
+        case "Oneens":
+            console.log("Oneens");
+            goToNextStatement();
+            break;
+        case "Sla deze vraag over":
+            console.log("Sla deze vraag over");
+            goToNextStatement();
             break;
         default:
             break;
     }
+}
+
+// Go to the next statement
+function goToNextStatement() {
+    currentSubject++;
+    changeContent(currentSubject);
+}
+
+// Go back to the previous statement
+function goToPreviousStatement() {
+    currentSubject--;
+    changeContent(currentSubject);
 }
