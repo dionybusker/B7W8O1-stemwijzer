fdcbc96cc5c836ef39d8165573c481373a1a1699
Diony <dionybusker@gmail.com>
2021-03-12 11:34:38 +0100

    Go to the next statement

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
-// Variables
+// Constants
 const startContainer = document.getElementById("startContainer");
 const statementContainer = document.getElementById("statementContainer");
 const startButton = document.getElementById("startButton");
 const title = document.getElementById("title");
 const statement = document.getElementById("statement");
+const nextButton = document.getElementById("nextButton");
 
+// Variables
 var currentSubject = 0;
 
 startButton.onclick = changeView;
+nextButton.onclick = switchStatement;
 
 
 // Add Bootstrap class "d-none"
@@ -24,17 +27,24 @@ function show(element) {
 function changeView() {
     hide(startContainer);
     show(statementContainer);
-    viewData();
+    changeContent(currentSubject);
 }
 
-function viewData() {
-    title.innerHTML = subjects[currentSubject].title;
-    statement.innerHTML = subjects[currentSubject].statement;
-
-    currentSubject++;
+function changeContent(currentSubject) {
+    if (currentSubject >= 0 && currentSubject < subjects.length) {
+        title.innerHTML = subjects[currentSubject].title;
+        statement.innerHTML = subjects[currentSubject].statement;
+    }
 }
 
 // Switch between multiple statements
 function switchStatement() {
-
+    switch (this.innerHTML) {
+        case "Next":
+            currentSubject++;
+            changeContent(currentSubject);
+            break;
+        default:
+            break;
+    }
 }
