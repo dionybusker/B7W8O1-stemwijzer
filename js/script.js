38be1cbbba4d1bf28a4e8a7f78d66d9146bd0a5e
Diony <dionybusker@gmail.com>
2021-03-24 21:32:18 +0100

    currentContainer counter (1)

--- js/script.js
+++ js/script.js
@@ -48,9 +48,9 @@ function changeView() {
     
     viewContent(currentSubject);
     
-    console.log(startButton);
+    // console.log("startButton: " + startButton);
     // console.log(currentSubject);
-    console.log(currentContainer);
+    console.log("currentContainer: " + currentContainer);
 }
 
 // Display the different statements
@@ -67,33 +67,33 @@ function switchStatement() {
 
     switch (this.id) {
         case "goBackButton":
-            console.log("Ga terug");
+            // console.log("Ga terug");
             goToPreviousStatement();
             break;
         case "proButton":
-            console.log("Eens");
+            // console.log("Eens");
             addUserVote("pro")
             goToNextStatement();
             break;
         case "neitherButton":
-            console.log("Geen van beide");
+            // console.log("Geen van beide");
             addUserVote("geen");
             goToNextStatement();
             break;
         case "contraButton":
-            console.log("Oneens");
+            // console.log("Oneens");
             addUserVote("contra");
             goToNextStatement();
             break;
         case "skipButton":
-            console.log("Sla deze vraag over");
+            // console.log("Sla deze vraag over");
             addUserVote("");
             goToNextStatement();
             break;
         default:
             break;
     }
-    console.log(subjects);
+    // console.log(subjects);
 }
 
 // Go to the next statement
@@ -110,32 +110,60 @@ function goToNextStatement() {
     //     hide(statementContainer);
     //     show(partyContainer);
     // }
-    console.log(currentContainer)
-    console.log(currentSubject)
+    console.log("currentSubject: " + currentSubject)
+    console.log("currentContainer: " + currentContainer)
 }
 
 // Go back to the previous statement
 function goToPreviousStatement() {
-    if (currentContainer != 0) {
+    /**
+     * deze functie wordt gebruikt op de "Ga terug"-knop
+     * de "Ga terug"-knop wordt weergegeven in de statementsContainer
+     * de "Ga terug"-knop wordt gebruikt om te kunnen navigeren tussen de statements (door naar de vorige statement(s) te gaan)
+     * 
+     */
+    // if (currentContainer != 0) {
         if (currentSubject > 0) {
             // show(statementContainer);
             currentSubject--;
+            
             viewContent(currentSubject);
         } else {
-            hide(statementContainer);
-            hide(goBackButton);
-            hide(partyContainer);
-            show(startContainer);
-        }
-        currentContainer--;
-    }
+            if (currentSubject == 0 && currentContainer == 1) {
+                currentContainer--;
 
-    if (currentSubject > 0 && currentSubject < subjects.length) {
-        hide(partyContainer);
-        show(statementContainer);
-    }
+                hide(statementContainer);
+                hide(goBackButton);
+                hide(partyContainer);
+                show(startContainer);
+            }
+        }
+    // }
+
+    // if (currentSubject > 0 && currentSubject < subjects.length) {
+    //     hide(partyContainer);
+    //     show(statementContainer);
+    // }
+
+    console.log("currentSubject: " + currentSubject)
+    console.log("currentContainer: " + currentContainer)
 }
 
 function addUserVote(vote) {
     subjects[currentSubject].vote = vote;
 }
+
+
+/**
+ * startContainer       => 0
+ * statementContainer   => 1
+ * partyContainer       => 2
+ * 
+ * currentSubject kan 0, 1, 2 of 3 zijn (met de huidige testdata)
+ * 
+ * op basis van de "Ga terug"-knop:
+ *   als currentSubject 0 is EN de currentContainer is 1 (statementContainer)
+ *     dan wordt currentContainer met 1 verminderd (currentContainer--)
+ * 
+ * 
+ */
